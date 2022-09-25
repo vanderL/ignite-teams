5ec83d2e4087f0d65d9fbddce95dc19c8d5e87dd
Vander Lima <vnd.vander@gmail.com>
2022-09-25 17:35:46 -0300

    [feat:] List Filters

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useState } from 'react';
+import { FlatList } from 'react-native';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
@@ -6,6 +8,13 @@ import { Input } from '@components/Input';
 import * as Sc from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState('TIme A');
+  const [players, setPlayers] = useState([]);
+
+  const playerTotal = useMemo(() => {
+    return players.length;
+  }, [players])
+
   return (
     <Sc.Container>
       <Header
@@ -26,10 +35,28 @@ export function Players() {
         />
       </Sc.Form>
 
-      <Filter
-        title='Time A'
-        isActive
-      />
+      <Sc.HeaderList>
+        <FlatList
+          data={['TIme A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+
+          horizontal
+        />
+
+        <Sc.NumberOfPlayers>
+          {playerTotal}
+        </Sc.NumberOfPlayers>
+
+      </Sc.HeaderList>
+
+
 
     </Sc.Container>
   )
